dfa35301c095b2c8f63c154d125b31aca86f8b04
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2017-02-20 11:43:02 +0100

    copy ssh config

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ DOTFILES="$HOME/.dotfiles"
 echo ". $DOTFILES/.bash_aliases" >>$HOME/.bashrc
 cat $DOTFILES/.gitalias >>$HOME/.gitconfig
 ln -s $DOTFILES/.gitignore_global $HOME/.gitignore_global
+cp $DOTFILES/ssh-config .ssh/config
+
